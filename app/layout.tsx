48ec212f7df681b652f5f74879facaac2e16a0a3
Supabi <susupaporn2248@gmail.com>
2026-09-23 14:38:04 +0700

    Update UI and Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import ApolloProvider from "@/components/ApolloProvider";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Search Pokemon",
